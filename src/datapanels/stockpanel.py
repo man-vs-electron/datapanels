661a67cf9e8b29da8ce2126c1ac35fb6f50ce248
Man vs Electron <manvselectron@gmail.com>
2021-11-20 16:21:12 -0700

    Integrated radio buttons

--- src/datapanels/stockpanel.py
+++ src/datapanels/stockpanel.py
@@ -8,32 +8,54 @@ from kivy.lang.builder import Builder
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 from kwidgets.dataviz.boxplot import BoxPlot
+from kwidgets.uix.radiobuttons import RadioButtons
 from kivy_garden.graph import Graph, MeshLinePlot
+
 
 
 Builder.load_string('''
 <StockPanel>:
     _boxplot: boxplot
     _graph: graph
+    _timeframe: timeframe
     orientation: 'horizontal'
     BoxLayout:
+        id: leftbox
         orientation: 'vertical'
         spacing: 10
         Label:
+            size_hint_y: .25
             halign: 'left'
-            text: root._ticker
+            valign: 'top'
+            text_size: self.width-20, self.height-20
+            text: '[b][size=20]'+root._ticker+'[/size][/b]'
+            markup: True
         Label:
+            size_hint_y: .25
             halign: 'left'
             text: root._ask
+        RadioButtons:
+            id: timeframe
+            size: leftbox.width, 30
+            size_hint: None, None
+            options: ["Month", "Year", "All"]
+            on_selected_value: root._timeframe_clicked(args[1])
         Graph:
+            size_hint_y: .5
             id: graph
             xlabel: 'time'
             ylabel: 'close'
             
-    BoxPlot:
-        id: boxplot
+    BoxLayout:
+        orientation: 'vertical'
         size_hint_x: .2
-        markercolor: 1, 0, 0, 1
+        BoxPlot:
+            id: boxplot
+            markercolor: 1, 0, 0, 1
+        Label:
+            size_hint_y: .25
+            halign: 'left'
+            text: root._max
 ''')
 
 
@@ -41,11 +63,17 @@ class StockPanel(BoxLayout):
     _period = StringProperty("1y")
     _ticker = StringProperty("Loading...")
     _ask = StringProperty("N/A")
+    _max = StringProperty("")
+    _3Q = StringProperty("")
+    _med = StringProperty("")
+    _1Q = StringProperty("")
+    _min = StringProperty("")
     _timer: Thread = None
     _running = True
     _update_rate_sec = NumericProperty(60*10)
     _boxplot = ObjectProperty(None)
     _graph = ObjectProperty(None)
+    _timeframe = ObjectProperty(None)
 
     def update_data(self):
         #try:
@@ -55,6 +83,12 @@ class StockPanel(BoxLayout):
         df = t.history(period=self._period)
         closes = list(df.Close)
         self._boxplot.data = closes
+        self._max = "Max: %0.2f\n3Q: %0.2f\nMed: %0.2f\n1Q: %0.2f\nMin: %0.2f" % \
+                    (self._boxplot._bpd.max,
+                     self._boxplot._bpd.q3,
+                     self._boxplot._bpd.median,
+                     self._boxplot._bpd.q1,
+                     self._boxplot._bpd.min)
         self._boxplot.markervalue = self._ask
 
         for p in list(self._graph.plots):
@@ -73,11 +107,17 @@ class StockPanel(BoxLayout):
         #    return False
 
 
+    def _update_now(self):
+        Thread(target=self.update_data, daemon=True).start()
+
     def _update_data_loop(self):
         while self._running:
             while not self.update_data():
                 sleep(10)
             sleep(self._update_rate_sec)
+
+    def _timeframe_clicked(self, newperiod):
+        print(newperiod)
 
     @property
     def ticker(self):
@@ -97,12 +137,14 @@ class StockPanel(BoxLayout):
     def update_rate_sec(self, rate: int):
         self._update_rate_sec = rate
 
+
 class StockPanelApp(App):
 
     def build(self):
         container = Builder.load_string('''
 StockPanel:
-    ticker: 'LUV'
+    size_hint_y: 1
+    ticker: 'PSEC'
     update_rate_sec: 60*10
 ''')
         return container
