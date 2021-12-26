ea2cc1672cc3f1e671d442305d164791166ee8e2
Man vs Electron <manvselectron@gmail.com>
2021-12-25 22:37:46 -0700

    Removed unused file

--- src/datapanels/globals.py
+++
@@ -1,6 +0,0 @@
-import threading
-
-# A global lock that gets acquired before changing the page and released afterwards.
-# Acquire and release this before doing complex drawing on a panel.
-ui_lock = threading.Lock()
-
